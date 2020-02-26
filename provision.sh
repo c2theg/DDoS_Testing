85be4f38f5178f8f7e16b55eaa98f61ffa81d2c5
Chris Gray <christophermjgray@gmail.com>
2020-02-25 22:31:22 -0500

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -18,21 +18,19 @@ Last Updated:  2/22/2020
 \r\n \r\n
 This is meant for Ubuntu 16.04+  \r\n \r\n"
 #---------------------------------------------------------------------------------------------------------
-if [ -s "update_attacks.sh" ]; then
-	echo "Deleting old configs...  "
-	rm install_gen_data.sh
-	rm gen_data.sh
-	rm update_attacks.sh
+if [ ! -d "attacks/" ]; then
+	mkdir attacks
+	cd attacks/
 fi
+
+echo "\r\n \r\n Downloading Attack Script Updater "
+wget -O "update_attacks.sh" "https://raw.githubusercontent.com/c2theg/DDoS_Testing/master/update_attacks.sh"
 
 echo "\r\n \r\n Downloading install_gen_data script.. "
 wget -O "install_gen_data.sh" "https://raw.githubusercontent.com/c2theg/DDoS_Testing/master/install_gen_data.sh"
 
 echo "\r\n \r\n Downloading Sample Data Generator.. "
 wget -O "gen_data.sh" "https://raw.githubusercontent.com/c2theg/DDoS_Testing/master/gen_data.sh"
-
-echo "\r\n \r\n Downloading Attack Script Updater "
-wget -O "update_attacks.sh" "https://raw.githubusercontent.com/c2theg/DDoS_Testing/master/update_attacks.sh"
 
 sudo chmod u+x *.sh
 
