180013cb67f25c1309115f6c54fbda3c1e5b84c1
Chris Gray <christophermjgray@gmail.com>
2020-03-02 14:14:40 -0500

    Add files via upload

--- kill_all_attacks.sh
+++ kill_all_attacks.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 # Christopher Gray
-#  Version 0.0.2
-#  2/26/2020
+#  Version 0.0.6
+#  3/2/2020
 
-killall -9 dnsperf
+echo "Killing all attack threads..."
+
+echo "HPing3... "
 killall -9 hping3
+wait
+sleep 1
+
+echo "NPing... "
 killall -9 nping
+wait
+sleep 1
+
+echo "Apache Bench (AB)... "
 killall -9 ab
+wait
+sleep 1
+
+echo "Various DNS Attacks..."
 killall -9 attack_dns_nxdomain.py
 killall -9 attack_phantomdomain.py
 killall -9 mz
@@ -14,3 +28,22 @@ killall -9 f5-dns-flood.py
 killall -9 attack_teardrop.py
 killall -9 attack_dns_watertorture_wget.sh
 killall -9 gen_udp_floods.sh
+wait
+sleep 2
+
+
+echo "DNS Pref.."
+killall -9 dnsperf
+
+
+echo "
+
+Done!
+
+
+Run the following to confirm:
+
+htop
+speedometer -l -r eth0 -t eth0 -m $(( 1024 * 1024 * 3 / 2 ))
+
+""
